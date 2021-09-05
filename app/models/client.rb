7bb7e5a3c352434341781400f9968c713486554f
THIAGO V <thaigovernetti@gmail.com>
2021-09-05 19:57:45 -0300

    criados index da página bank accounts, show do bank accounts e algumas correções

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,15 +2,14 @@ class Client < ApplicationRecord
     validates :name, presence: true
     validates :last_name, presence: true
     validates :client_number, presence: true, uniqueness: true
-    validates_associated :content
 
     has_many :bank_accounts
 
     before_save :format_name
 
     def format_name
-        self.first_name = self.first_name.upcase
-        self.last_name = self.first_name.upcase
+        self.name = self.name.upcase
+        self.last_name = self.last_name.upcase
     end
 
     def num_accounts
